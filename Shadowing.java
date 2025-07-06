1966c10ddb6ebd9577ee44c2ec6857a84159ae0a
Omkar <omkarsalunke1978@gmail.com>
2025-07-06 21:23:41 +0530

    Daily_06-07-2025

---
+++ Shadowing.java
@@ -0,0 +1,23 @@
+public class Shadowing {
+    static int x = 10;
+    public static void main(String[] args) {
+        System.out.println(x);
+        // int x = 349;     
+        // the shadowing doesn't begin when you declare a variable, it begins when you initialize it.
+        int x;      // declaring. Will give error below if not initialized, since they will try to use this now.  
+        x = 987;
+        System.out.println(x);
+        fun();      // will take the static int x
+        fun2(x);    // will take the above newly initialized value (x got shadowed)
+    }
+
+    static void fun()
+    {
+        System.out.println(x);
+    }
+
+    static void fun2(int x)
+    {
+        System.out.println(x);
+    }
+}
