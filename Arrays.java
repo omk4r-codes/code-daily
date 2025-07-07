808aa5917dbbff8ab862bba956dded2eaa89e914
Omkar <omkarsalunke1978@gmail.com>
2025-07-07 23:02:54 +0530

    Daily_07_07_2025

---
+++ Arrays.java
@@ -0,0 +1,31 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+       // syntax:
+       int[] arr = {1,2,3,4,5}; 
+       int[] roll = new int[8]; 
+
+       // by default, int array initialized by 0
+       for (int i : roll) {
+        System.out.print(i + ", ");
+       }
+       System.out.println();
+
+       roll[0] = 1;
+       roll[1] = 1;
+       roll[2] = 1;
+       roll[3] = 1;
+       roll[4] = 1;
+       
+
+       for (int i : roll) {
+        System.out.print(i + ", ");
+       }
+
+       char[] ch = new char[4];
+       // no char for default char array
+       for (char c : ch) {
+        System.out.print(c + ", ");
+       }
+    }
+}
