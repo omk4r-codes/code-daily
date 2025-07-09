14754f2d05e3c7a7c0724306fd665d73436a366d
Omkar <omkarsalunke1978@gmail.com>
2025-07-10 00:47:16 +0530

    Daily_09_07_2025

---
+++ Arraylists.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+// slower than array
+
+public class Arraylists {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // ArrayList<Integer> arr = new ArrayList<Integer>();
+        // or: 
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        // we can give initial capacity in the brackets:
+        ArrayList <Integer> arr2 = new ArrayList<>(5);
+
+        // or you can just skip the type (not a good practice):
+        ArrayList arr4 = new ArrayList();
+
+
+        // addition:
+        arr.add(1);
+        arr.add(3);
+        arr.add(4);
+        arr.add(8);
+
+        // print / access index:
+        for(int i = 0; i < arr.size(); i++){
+            System.out.println(arr.get(i));
+            // note that we use .size() in arrayList and length in arrays.
+            // note that we use arr.get(i) to access the element by index; arr[i] doesn't work here!
+        }
+        
+        for (int it : arr) {
+            System.out.println(it);
+        }
+
+
+        // arraylist of arraylists:
+        ArrayList<ArrayList<Integer>> arr5 = new ArrayList<>();
+        // at this moment, everything is null in arr5:
+        System.out.println(arr5);       // --> []
+        // we have to initialize arraylists in it:
+        for(int i = 0; i < 3; i++){
+            arr5.add(new ArrayList<>());
+        }
+        // now 3 arraylists have been added to the arr5:
+        System.out.println(arr5);       // --> [[], [], []]
+
+        // input :
+        for(int i = 0; i < arr5.size(); i++){
+            for(int j = 0; j < 3; j++){
+                arr5.get(i).add(sc.nextInt());
+            }
+        }
+
+        System.out.println(arr5);
+    }
+}
---
+++ TwoDArray.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][4];
+        // we can also declare like this since individual array size may vary:
+        // int[][] arr = new int[3][];
+
+        // also like this:
+        int[][] arr2 = {
+            {1,2,3,4},
+            {1,4},
+            {1,2,3}
+        };
+
+        // printing
+        for (int[] it : arr2) {
+            System.out.print(Arrays.toString(it) + " ");
+        }
+        System.out.println();
+
+        for(int i = 0; i < arr2.length; i++){
+            for (int it : arr2[i]) {
+                System.out.print(it + ", ");
+            }
+            System.out.println();
+        }
+
+        // input:
+        int[][] arr3 = new int[4][5];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < arr3.length; i++){
+            for(int j = 0; j < arr3[i].length; j++){
+                System.out.print("enter element " + i + ", " + j + " : ");
+                arr3[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < arr3.length; i++){
+            for(int j = 0; j < arr3[i].length; j++){
+                System.out.print(arr3[i][j] + ", ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
